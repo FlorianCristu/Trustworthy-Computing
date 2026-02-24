b7656d42ec85ccd3aec49df036ac1a22444e98ac
FlorianCristu <114191133+FlorianCristu@users.noreply.github.com>
2026-02-24 01:19:47 +0000

    big first commit
    this is a first commit for this coursework

---
+++ ACW1-SandboxTool/HelloFlorian/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloFlorian
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello ");
+
+            string input = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("World");
+            }
+            else
+            {
+                Console.WriteLine(input);
+            }
+        }
+    }
+}
